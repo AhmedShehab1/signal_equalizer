223fbec94ae786d0d2d3628a3e67d0c4ca4bdc44
info <info@shehabtech.me>
2025-11-13 13:10:37 +0200

    feat: Add CustomizedMode component exports for streamlined imports

---
+++ frontend/src/components/CustomizedMode/index.ts
@@ -0,0 +1,42 @@
+/**
+ * CustomizedMode Component Exports
+ * 
+ * Central export point for all CustomizedMode subcomponents, hooks, and types.
+ * Simplifies imports throughout the application.
+ * 
+ * @example
+ * ```tsx
+ * import { ModeToggle, ContentTypeSelector, useAudioMixing } from './components/CustomizedMode';
+ * ```
+ */
+
+// Main orchestrator (to be refactored)
+export { default as CustomizedModePanel } from '../CustomizedModePanel';
+
+// Presentational Components
+export { ModeToggle } from './ModeToggle';
+export { ContentTypeSelector } from './ContentTypeSelector';
+export { AIControls } from './AIControls';
+export { SourceCard } from './SourceCard';
+export { SourceList } from './SourceList';
+
+// Custom Hooks
+export { useAudioMixing } from './hooks/useAudioMixing';
+export { useAudioPlayback } from './hooks/useAudioPlayback';
+
+// Types
+export type {
+  ProcessingMode,
+  ContentType,
+  ModelType,
+  AISeparationCache,
+  CustomizedModePanelProps,
+  ModeToggleProps,
+  ContentTypeSelectorProps,
+  AIControlsProps,
+  SourceCardProps,
+  SourceListProps,
+  UseAudioMixingReturn,
+  UseAudioPlaybackReturn,
+  AudioSourceMetadata
+} from './types';
